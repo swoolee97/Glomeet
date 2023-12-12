cdb5d906322f4ef7486eb3edefb24add86255a71
swoolee97 <swoolee97@ajou.ac.kr>
2023-12-12 22:48:15 +0900

    프로젝트 생성

---
+++ Desktop/podo/src/test/java/com/podo/PodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
